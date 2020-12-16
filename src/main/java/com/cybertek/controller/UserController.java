eb5c4235f45b4e4e8323f3e1537382cecf63928f
MrtYvz192 <wingsoverthesky@gmail.com>
2020-12-17 01:49:04 +0300

    "User Create Delete"

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -68,4 +68,10 @@ public class UserController {
         return "redirect:/user/create";
     }
 
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username, Model model){
+        dataGenerator.userService.deleteById(username);
+
+        return "redirect:/user/create";
+    }
 }
