c79bcbe00119a65188847e3041911951d4cf340a
MrtYvz192 <wingsoverthesky@gmail.com>
2020-12-19 01:11:34 +0300

    "Project Create"

--- src/main/java/com/cybertek/controller/ProjectController.java
+++ src/main/java/com/cybertek/controller/ProjectController.java
@@ -2,12 +2,15 @@ package com.cybertek.controller;
 
 
 import com.cybertek.dto.ProjectDTO;
+import com.cybertek.enums.Status;
 import com.cybertek.service.ProjectService;
 import com.cybertek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -28,6 +31,22 @@ public class ProjectController {
         model.addAttribute("managers", userService.findAll());
 
         return "project/create";
+    }
+
+    @PostMapping("/create")
+    public String insertProject(ProjectDTO project){
+        project.setProjectStatus(Status.OPEN);
+        projectService.save(project);
+
+        return "redirect:/project/create";
+    }
+
+    @GetMapping("/delete/{projectcode}")
+    public String deleteProject(@PathVariable("projectcode") String projectCode){
+        projectService.deleteById(projectCode);
+
+
+        return "redirect:/project/create";
     }
 
 
